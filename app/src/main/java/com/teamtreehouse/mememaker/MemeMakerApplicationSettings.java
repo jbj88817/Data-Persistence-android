2c231e004a52123f8daf6285e3d17786285a7566
Bojie Jiang <jiangbojie@gmail.com>
2014-12-10 22:52:38 -0800

    project update part 3

--- app/src/main/java/com/teamtreehouse/mememaker/MemeMakerApplicationSettings.java
+++ app/src/main/java/com/teamtreehouse/mememaker/MemeMakerApplicationSettings.java
@@ -11,7 +11,22 @@ import com.teamtreehouse.mememaker.utils.StorageType;
  */
 public class MemeMakerApplicationSettings {
 
+    SharedPreferences mSharedPreferences;
+
     public MemeMakerApplicationSettings(Context context) {
 
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+
+    }
+
+    public String getStoragePreference() {
+        return mSharedPreferences.getString("Storage", StorageType.INTERNAL);
+    }
+
+    public void setSharedPreferences(String storageType) {
+        mSharedPreferences
+                .edit()
+                .putString("Storage", storageType)
+                .apply();
     }
 }
--- app/src/main/java/com/teamtreehouse/mememaker/ui/fragments/MemeSettingsFragment.java
+++ app/src/main/java/com/teamtreehouse/mememaker/ui/fragments/MemeSettingsFragment.java
@@ -9,5 +9,10 @@ import com.teamtreehouse.mememaker.R;
  * Created by Evan Anger on 8/13/14.
  */
 public class MemeSettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
 
+    }
 }
