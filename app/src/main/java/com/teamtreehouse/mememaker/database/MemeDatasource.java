4a85c9d0144adc12231a2b145c78a1ee6ee526f0
Bojie Jiang <jiangbojie@gmail.com>
2014-12-19 08:36:14 -0800

    project update

--- app/src/main/java/com/teamtreehouse/mememaker/database/MemeDatasource.java
+++ app/src/main/java/com/teamtreehouse/mememaker/database/MemeDatasource.java
@@ -1,16 +1,6 @@
 package com.teamtreehouse.mememaker.database;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.provider.BaseColumns;
-
-import com.teamtreehouse.mememaker.models.Meme;
-import com.teamtreehouse.mememaker.models.MemeAnnotation;
-
-import java.util.ArrayList;
-import java.util.Date;
 
 /**
  * Created by Evan Anger on 8/17/14.
@@ -22,5 +12,6 @@ public class MemeDatasource {
 
     public MemeDatasource(Context context) {
         mContext = context;
+        mMemeSqlLiteHelper = new MemeSQLiteHelper(context);
     }
 }
